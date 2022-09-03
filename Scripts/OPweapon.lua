4b6886ce9ebaab263c4584bd1296c2d4155b2084
Karl Wheezer D. God <110885234+InfernoKarl@users.noreply.github.com>
2022-09-02 23:50:30 -0700

    removed divinity, isnt as good as redcliff

--- Scripts/OPweapon.lua
+++ Scripts/OPweapon.lua
@@ -1,16 +1,8 @@
 local args = {
     [1] = "Buy",
-    [2] = "Shards",
-    [3] = -1000000000000000000,
-    [4] = "Divinity"
-}
-
-local args2 = {
-    [1] = "Buy",
     [2] = "Gold",
     [3] = 0,
     [4] = "Redcliff Pacifier"
 }
 
 game:GetService("Players").LocalPlayer.RemoteFunctions.SwordSystem:FireServer(unpack(args))
-game:GetService("Players").LocalPlayer.RemoteFunctions.SwordSystem:FireServer(unpack(args2))
